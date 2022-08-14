68dc9c4ca158ed5b825283016d93a731382aaf38
Sarah Packowski <spackows@ca.ibm.com>
2022-08-14 16:18:05 -0400

    Add chatbot pieces

--- server.js
+++ server.js
@@ -10,6 +10,9 @@ const g_express    = require( "express"     );
 const g_axios      = require( "axios"       );
 
 
+const g_log  = require( "./my_log.js" );
+
+
 var g_app = g_express();
 g_app.use( g_bodyParser.json() );
 g_app.use( g_bodyParser.urlencoded( { extended: true } ) );
@@ -18,32 +21,21 @@ g_app.use( g_bodyParser.urlencoded( { extended: true } ) );
 const g_server = g_http.Server( g_app );
 g_server.listen( 8080, function()
 {
-  console.log( "[server] Server running" );
+  g_log.writeLog( "[server] Server running" );
   
-});
-
-
-g_app.get( "/health", function( request, response )
-{
-    // Needed for Code Engine pipeline
-    
-    console.log( "[server] /health ..." );
-    
-    response.status( 200 ).end( "Success" );
-    
 } );
 
 
 g_app.post( "/webhook-endpoint", function( request, response )
 {
-    console.log( "[server] /webhook-endpoint ..." );
-    console.log( "[server] /webhook-endpoint body:\n" + JSON.stringify( request.body, null, 3 ) );
+    g_log.writeLog( "[server] /webhook-endpoint ..." );
+    g_log.writeLog( "[server] /webhook-endpoint body:\n" + JSON.stringify( request.body, null, 3 ) );
     
     var action     = request.body.action     ? request.body.action     : "";
     var session_id = request.body.session_id ? request.body.session_id : "";
     
-    console.log( "[server] /webhook-endpoint action:     " + action     );
-    console.log( "[server] /webhook-endpoint session_id: " + session_id );
+    g_log.writeLog( "[server] /webhook-endpoint action:     " + action     );
+    g_log.writeLog( "[server] /webhook-endpoint session_id: " + session_id );
     
     switch( action )
     {
@@ -89,9 +81,9 @@ g_app.post( "/webhook-endpoint", function( request, response )
 
 function sendMessage( session_id, msg )
 {
-    console.log( "[server] sendMessage ..." );
-    console.log( "[server] sendMessage session_id: " + session_id );
-    console.log( "[server] sendMessage msg: " + msg );
+    g_log.writeLog( "[server] sendMessage ..." );
+    g_log.writeLog( "[server] sendMessage session_id: " + session_id );
+    g_log.writeLog( "[server] sendMessage msg: " + msg );
     
     return;
 
@@ -106,18 +98,18 @@ function sendMessage( session_id, msg )
     var headers = { "Content-Type" : "application/json" };
     var parms   = { "auth" : auth, "headers" : headers };
     
-    console.log( "[server] sendMessage:\n" +
+    g_log.writeLog( "[server] sendMessage:\n" +
                  "url: " + url + "\n" +
                  "body:\n" + JSON.stringify( body, null, 3 ) + "\n" +
                  "parms:\n" + JSON.stringify( parms, null, 3 ) );
     
     g_axios.post( url, data, parms ).then( function( data )
     {
-        console.log( "[server] sendMessage: Message sent successfully" );
+        g_log.writeLog( "[server] sendMessage: Message sent successfully" );
         
     } ).catch( function( error )
     {
-        console.log( "[server] sendMessage: Error" );
+        g_log.writeLog( "[server] sendMessage: Error" );
         
         printAxiosError( error );
         
@@ -130,30 +122,30 @@ function printAxiosError( error )
     // https://www.npmjs.com/package/axios#handling-errors
     //
     
-    console.log( "\nAxios error:" );
+    g_log.writeLog( "\nAxios error:" );
     
     if( error.response )
     {
         // The request was made and the server responded with a status code
         // that falls out of the range of 2xx
-        console.log( error.response.data    );
-        console.log( error.response.status  );
-        console.log( error.response.headers );
+        g_log.writeLog( error.response.data    );
+        g_log.writeLog( error.response.status  );
+        g_log.writeLog( error.response.headers );
     } 
     else if( error.request )
     {
         // The request was made but no response was received
         // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
         // http.ClientRequest in node.js
-        console.log( error.request );
+        g_log.writeLog( error.request );
     } 
     else 
     {
       // Something happened in setting up the request that triggered an Error
-      console.log( error.message );
+      g_log.writeLog( error.message );
     }
     
-    console.log( error.config );
+    g_log.writeLog( error.config );
 }
 
 
@@ -161,13 +153,13 @@ function printAxiosError( error )
 
 g_app.post( "/slow-endpoint", function( request, response )
 {
-    console.log( "[server] /slow-endpoint ..." );
+    g_log.writeLog( "[server] /slow-endpoint ..." );
     
     const myTimeout = setTimeout( function()
     {
         // Wait 10 seconds before responding
         
-        console.log( "[server] /slow-endpoint responds" );
+        g_log.writeLog( "[server] /slow-endpoint responds" );
         
         response.status( 200 ).json( { "error_str" : "", "result" : "Success" } );
         
@@ -178,13 +170,13 @@ g_app.post( "/slow-endpoint", function( request, response )
 
 g_app.post( "/async-endpoint", function( request, response )
 {
-    console.log( "[server] /async-endpoint ..." );
+    g_log.writeLog( "[server] /async-endpoint ..." );
     
     startJob( function( job_id )
     {
         var result = { "error_str" : "", "job_id" : job_id };
         
-        console.log( "[server] /async-endpoint result:\n" + JSON.stringify( result, null, 3 ) );
+        g_log.writeLog( "[server] /async-endpoint result:\n" + JSON.stringify( result, null, 3 ) );
         
         response.status( 200 ).json( result );
         
@@ -195,18 +187,18 @@ g_app.post( "/async-endpoint", function( request, response )
 
 g_app.post( "/polling-endpoint", function( request, response )
 {
-    console.log( "[server] /polling-endpoint ..." );
-    console.log( "[server] /polling-endpoint body:\n" + JSON.stringify( request.body, null, 3 ) );
+    g_log.writeLog( "[server] /polling-endpoint ..." );
+    g_log.writeLog( "[server] /polling-endpoint body:\n" + JSON.stringify( request.body, null, 3 ) );
     
     var job_id = request.body.job_id ? request.body.job_id : "";
     
-    console.log( "[server] /polling-endpoint job_id: " + job_id );
+    g_log.writeLog( "[server] /polling-endpoint job_id: " + job_id );
     
     if( !job_id || !( job_id in g_jobs_json ) )
     {
         var result = { "error_str" : "No valid job_id found in request", "state" : "Error" };
         
-        console.log( "[server] /polling-endpoint result:\n" + JSON.stringify( result, null, 3 ) );
+        g_log.writeLog( "[server] /polling-endpoint result:\n" + JSON.stringify( result, null, 3 ) );
         
         response.status( 400 ).json( result );
         
@@ -215,7 +207,7 @@ g_app.post( "/polling-endpoint", function( request, response )
     
     var result = { "error_str" : "", "state" : g_jobs_json[ job_id ] };
     
-    console.log( "[server] /polling-endpoint result:\n" + JSON.stringify( result, null, 3 ) );
+    g_log.writeLog( "[server] /polling-endpoint result:\n" + JSON.stringify( result, null, 3 ) );
     
     response.status( 200 ).json( result );
         
@@ -227,11 +219,11 @@ var g_jobs_json = {};
 
 function startJob( callback )
 {
-    console.log( "[server] startJob ..." );
+    g_log.writeLog( "[server] startJob ..." );
     
     var job_id = new Date().getTime().toString();
     
-    console.log( "[server] startJob job_id: " + job_id );
+    g_log.writeLog( "[server] startJob job_id: " + job_id );
     
     callback( job_id );
     
@@ -239,13 +231,13 @@ function startJob( callback )
     
     const myTimeout1 = setTimeout( function()
     {
-        console.log( "[server] startJob - processing job_id: " + job_id );
+        g_log.writeLog( "[server] startJob - processing job_id: " + job_id );
         
         g_jobs_json[ job_id ] = "processing";
         
         const myTimeout2 = setTimeout( function()
         {
-            console.log( "[server] startJob - done job_id: " + job_id );
+            g_log.writeLog( "[server] startJob - done job_id: " + job_id );
             
             g_jobs_json[ job_id ] = "done";
             
@@ -259,27 +251,66 @@ function startJob( callback )
 
 setInterval( function()
 {
-    console.log( "[server] cleanup ..." );
+    g_log.writeLog( "[server] cleanup ..." );
     
     for( job_id in g_jobs_json )
     {
         if( "done" == g_jobs_json[ job_id ] )
         {
-            console.log( "[server] cleanup - done job_id: " + job_id );
+            g_log.writeLog( "[server] cleanup - done job_id: " + job_id );
             g_jobs_json[ job_id ] = "stale";
             continue;
         }
         
         if( "stale" == g_jobs_json[ job_id ] )
         {
-            console.log( "[server] cleanup - stale job_id: " + job_id );
+            g_log.writeLog( "[server] cleanup - stale job_id: " + job_id );
             g_jobs_json[ job_id ] = null;
             delete g_jobs_json[ job_id ];
         }
     }
     
-    console.log( "[server] cleanup g_jobs_json:\n" + JSON.stringify( g_jobs_json, null, 3 ) );
+    g_log.writeLog( "[server] cleanup g_jobs_json:\n" + JSON.stringify( g_jobs_json, null, 3 ) );
     
 }, 5 * 60 * 1000 );
+
+
+g_app.get( "/logs", function( request, response )
+{
+    g_log.readLog( function( content )
+    {
+        response.status( 200 ).end( content );
+        
+    } );
+        
+} );
+
+
+g_app.post( "/clearlog", function( request, response )
+{
+    g_log.clearLog( function( error_str )
+    {
+        if( error_str )
+        {
+            response.status( 500 ).json( { error_str : error_str } );
+            return;
+        }
+        
+        response.status( 200 ).json( { "result" : "Success" } );
+        
+    } );
+        
+} );
+
+
+g_app.get( "/health", function( request, response )
+{
+    // For Code Engine pipeline to check
+    
+    g_log.writeLog( "[server] /health ..." );
+    
+    response.status( 200 ).end( "Success" );
+    
+} );
 
 
